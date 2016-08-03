525b1ecd466572d65fdf7b1efca238d42f7ce8b7
WyomingCPA <WyomingCPA@yandex.ru>
2016-08-03 12:12:29 +0300

    Refresh Model Balance

--- SMSTracker/balance/models.py
+++ SMSTracker/balance/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 from datetime import *
 
 class UserBalanceChange(models.Model):
-    SERVICE1 = 0
-    SERVICE2 = 1
-    SERVICE3 = 2
-    NO_REASON = 3
+    SERVICE1 = 'SR1'
+    SERVICE2 = 'SR2'
+    SERVICE3 = 'SR3'
+    NO_REASON = 'NR'
     REASON_CHOICES = (
         (SERVICE1, 'Service1'),
         (SERVICE2, 'Service2'),
@@ -15,6 +15,6 @@ class UserBalanceChange(models.Model):
     )
 
     user = models.ForeignKey(User, related_name='balance_changes')
-    reason = models.IntegerField(choices=REASON_CHOICES, default=NO_REASON)
+    reason = models.CharField(choices=REASON_CHOICES, default=NO_REASON, max_length=7)
     amount = models.DecimalField(default=0, max_digits=18, decimal_places=6)
     datetime = models.DateTimeField(default=datetime.now())
